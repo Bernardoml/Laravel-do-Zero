aa76755820ba227b21be2205358e007daba9fa75
Bernardoml <74575193+Bernardoml@users.noreply.github.com>
2021-09-07 10:46:22 -0300

    adicionanto todos os assests e criando as paginas do site de maneira estatica

--- routes/web.php
+++ routes/web.php
@@ -24,16 +24,16 @@ use App\Http\Controllers\Site\ContactController;
 //     Route::get('produtos/{slug}', [CategoryController::class, 'show']);
 // });
 
-Route::get('/', HomeController::class);
+Route::get('/', HomeController::class)->name('site.home');
 
-Route::get('produtos', [CategoryController::class, 'index']);
-Route::get('produtos/{slug}', [CategoryController::class, 'show']);
+Route::get('produtos', [CategoryController::class, 'index'])->name('site.products');
+Route::get('produtos/{slug}', [CategoryController::class, 'show'])->name('site.products.category');
 
 // criou a classe invokable para retornar apenas a view direto
-Route::get('blog', BlogController::class);
+Route::get('blog', BlogController::class)->name('site.blog');
 
 // criando uma view direto, pois a página sobre é estática
-Route::view('sobre', 'site.about.index');
+Route::view('sobre', 'site.about.index')->name('site.about');
 
-Route::get('contato', [ContactController::class, 'index']);
-Route::post('contato', [ContactController::class, 'form']);
+Route::get('contato', [ContactController::class, 'index'])->name('site.contact');
+Route::post('contato', [ContactController::class, 'form'])->name('site.contact.form');
